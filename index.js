f16251fff6b2db36da7e66efcd0681432cf9a5af
JorgeMarinC <jorgemarincordero@gmail.com>
2021-03-13 02:11:02 +0100

    Add index file and endpoint

--- index.js
+++ index.js
@@ -8,10 +8,20 @@ var express = require("express");
 // Create application
 var app = express();
 
+// Path instantiation
+var path = require('path');
+
 // Define listen port
 var port = 10000;
 
 // Declare endpoints
+
+// Returns the index page
+app.get("/", (request, response) => {
+    response.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Returns a ASSCII funny face
 app.get("/cool", (request, response) => {
     response.send(cool());
     // Get the ip address of the client
@@ -19,6 +29,7 @@ app.get("/cool", (request, response) => {
     console.log("New request from " + remote_ip);
 
 });
+
 
 
 // Run the application on the defined port
